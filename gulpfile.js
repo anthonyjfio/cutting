00085e55a09fce75041937d0516836b3fb48d488
Anthony <ajfiorani@gmail.com>
2015-07-18 19:12:24 -0400

    refactor code based on better ref article

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,31 @@
-var gulp         = require('gulp'),
-    gutil        = require('gulp-util'),
-    jade         = require('gulp-jade'),
-    stylus       = require('gulp-stylus'),
-    fileinclude  = require('gulp-file-include'),
-    rename       = require('gulp-rename'),
-    notify       = require('gulp-notify'),
-    livereload   = require('gulp-livereload'),
-    lr           = require('tiny-lr'),
-    connect      = require('gulp-connect'),
-    plumber      = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer')
-    server       = lr(),
-    path         = require('path');
+var gulp = require('gulp'),
+    $    = require('gulp-load-plugins')();
 
 var paths = {
-  templates: './templates/',
-  stylus: './src/*.styl'
+  jadesrc: './src/**/*.jade',
+  stylussrc: './src/**/*.styl',
+  destination: './build'
 };
 
-gulp.task('fileinclude', function() {
-  return gulp.src(path.join(paths.templates, '*.jade'))
-    .pipe(fileinclude())
-    .pipe(rename({
-      extname: ""
-    }))
-    .pipe(rename({
-      extname: ".html"
-    }))
-    .pipe(gulp.dest('./'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Includes: included' }));
-});
-
-gulp.task('jade', function() {
-  return gulp.src(path.join(paths.templates, '*.jade'))
-    .pipe(plumber())
-    .pipe(jade())
-    .pipe(gulp.dest('./build/'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Jade files have reloaded' }));
-});
-
-gulp.task('stylus', function() {
-  gulp.src('./build/main.styl')
-    .pipe(stylus())
+gulp.task('html', function() {
+  gulp.src(paths.jadesrc)
+    .pipe($.jade({ doctype: 'html' }))
+    .on('error', $.util.log)
     .pipe(gulp.dest('./build'));
 });
 
-/*gulp.task('stylus', function() {
-  return gulp.src(path.join(paths.stylus, '*.styl'))
-    .pipe(plumber())
-    .pipe(stylus())
-    .pipe(autoprefixer({
-      browsers: ['last 2 version', "> 1%", 'ie 8', 'ie 9'],
-      cascade: false
-      }))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Stylus files have reloaded' }));
-});*/
-
-gulp.task('connect', function() {
-  connect.server({
-    port: 1337,
-    root: [__dirname],
-    livereload: false
-  });
+gulp.task('css', function() {
+  gulp.src(paths.stylussrc)
+    .pipe($.stylus({ compress: true }))
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest(paths.destination));
 });
-
-function watchStuff(task) {
-  server.listen(35729, function(err) {
-    if (err) {
-      return console.log(err);
-    }; 
-    gulp.watch(path.join(paths.stylus, '**/*.stylus'), [task]);
-    gulp.watch(path.join(paths.templates, '**/*.jade'), ['fileinclude']);
-    // NOTE: If above doesn't work try switching jade to html
-  });
-};
 
 gulp.task('watch', function() {
-  watchStuff('stylus');
+  gulp.watch(paths.jadesrc, ['html']);
+  gulp.watch(paths.stylussrc, ['css']);
 });
 
-gulp.task('default', ['fileinclude', 'stylus', 'connect', 'watch'], function() {
-});
+gulp.task('run', ['html', 'css', 'watch'], function() {
 
-// test
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 [Ref Article](https://medium.com/objects-in-space/considering-a-static-site-tool-learn-gulp-2fd5f9821fc4)
+
 [Ref gist from article above](https://gist.github.com/andrewdc/10659265)
+
+[Better ref article](https://www.codementor.io/development-process/tutorial/how-to-set-up-gulp-beginner-guide)
