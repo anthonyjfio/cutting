58c499f4d73f5039e5f0a02e65d5efa3f6b1641e
Anthony <ajfiorani@gmail.com>
2015-08-31 21:37:01 -0400

    overhaul; add critical; remove image-inline; add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,84 @@
 var gulp = require('gulp')
+  , critical = require('critical')
   , $    = require('gulp-load-plugins')();
 
 var paths = {
   jadesrc: './src/**/*.jade',
   stylussrc: './src/**/*.styl',
-  destination: './build'
+  normalizesrc: './src/css/vendor/normalize-css/normalize.css',
+  imagesrc: [
+    './src/**/*.jpeg',
+    './src/**/*.jpg',
+    './src/**/*.svg',
+    './src/**/*.png',
+    './src/**/*.gif'
+  ],
+  destination: './build/',
+  htmlsrc: './build/index.html',
+  csssrc: './build/css/main.css'
 };
 
 gulp.task('html', function() {
   gulp.src(paths.jadesrc)
     .pipe($.jade({ doctype: 'html' }))
-    .pipe($.inlineImageHtml('./src'))
     .pipe($.minifyInline())
     .on('error', $.util.log)
     .pipe(gulp.dest(paths.destination));
 });
 
 gulp.task('css', function() {
-  gulp.src(paths.stylussrc)
+
+  // See todo section of readme for how to use filter
+  // var Filter = $.filter(paths.stylussrc);
+
+  gulp.src([
+      paths.stylussrc,
+      paths.normalizesrc
+    ])
+    // .pipe(Filter)
     .pipe($.stylus({ compress: true }))
+    // .pipe(Filter.restore)
+    .pipe($.concat('main.css'))
     .pipe($.uncss({ html: ['./build/**/*.html'] }))
     .pipe($.autoprefixer())
     .pipe($.minifyCss())
     .pipe(gulp.dest(paths.destination));
+});
+
+gulp.task('image', function() {
+  gulp.src(paths.imagesrc)
+    .pipe($.imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      iterlaced: true
+    }))
+    .pipe(gulp.dest(paths.destination));
+});
+
+gulp.task('critical', ['build'], function() {
+  critical.generate({
+    base: './build/',
+    src: 'index.html',
+    dest: './build/critical.html',
+    minify: true,
+    dimensions: [{
+      width: 350,
+      height: 575
+    }, {
+      width: 500,
+      height: 575
+    }, {
+      width: 1300,
+      height: 900
+    }]
+  }, function(err, output) {
+    critical.inline({
+      base: './build',
+      src: 'index.html',
+      dest: './build/index.html',
+      minify: true
+      })
+  })
 });
 
 gulp.task('watch', function() {
@@ -30,6 +86,11 @@ gulp.task('watch', function() {
   gulp.watch(paths.stylussrc, ['css']);
 });
 
-gulp.task('default', ['html', 'css', 'watch'], function() {
+
+gulp.task('build', ['html', 'css', 'image'], function() {
+
+});
+
+gulp.task('default', ['build', 'critical', 'watch'], function() {
 
 });
--- readme.md
+++ readme.md
@@ -1,5 +1,21 @@
-[Ref Article](https://medium.com/objects-in-space/considering-a-static-site-tool-learn-gulp-2fd5f9821fc4)
+[ef Article](https://medium.com/objects-in-space/considering-a-static-site-tool-learn-gulp-2fd5f9821fc4)
 
 [Ref gist from article above](https://gist.github.com/andrewdc/10659265)
 
 [Better ref article](https://www.codementor.io/development-process/tutorial/how-to-set-up-gulp-beginner-guide)
+
+### Todo
+
+* ~~Inline critical css~~
+* ~~Preprocess jade and stylus ~~
+* ~~Autoprefix css~~
+* Minify html and css using multiple processors
+* remove unused css ( double check to ensure possible use with bootstrap and normalize)
+* watch for changes
+* livereload on change
+* add normalize css and use uncss on it
+    * See link below
+* concatenate all css onto one stylesheet
+    * http://stackoverflow.com/questions/25789127/gulp-compile-stylus-and-concat-with-pure-css
+* gulp-cssstats if < 10k use criticalcss else inline whole stylesheet
+* ~~look at https://github.com/addyosmani/critical-path-css-demo gulpfile on how to cache and optimize images~~
